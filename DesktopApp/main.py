5712499fdd836ccafd3dc5e7f71fd5c72cf3725c
VasilPaligorov <vpaligorov@gmail.com>
2021-04-22 21:05:41 +0300

    basic changes maded

--- DesktopApp/main.py
+++ DesktopApp/main.py
@@ -1,10 +1,19 @@
 from tkinter import *
-from tkinter import ttk
-from tkinter import messagebox
+from tkinter import ttk, messagebox, Text
 
 root = Tk()
 root.title("Pharmacy Machine")
-root.geometry("825x430")
+
+app_width = 1020
+app_height = 470
+
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+x = (screen_width / 2) - (app_width / 2)
+y = (screen_height / 2) - (app_height / 2)
+
+root.geometry(f'{app_width}x{app_height}+{int(x)}+{int(y)}')
 
 global index
 index = 0.0
@@ -12,21 +21,21 @@ index = 0.0
 global List
 List = []
 
-TextBox = Text(root, state="disabled", width=100, height=20)
+TextBox = Text(root, state="disabled", width=110, height=20, font=60)
 TextBox.grid(row=1, column=1, columnspan=5, padx=10)
 
 
 def add():
-    if Combo1.get() != "Choose option:" and Combo2.get() != "Choose option:":
+    if Combo1.get() != "Choose medicine:" and Combo2.get() != "Choose number:":
         global List
         global index
-        global counter
+        global number
         index += 1.0
         TextBox.configure(state='normal')
         TextBox.insert(float(index), str(Combo1.get()) + " x" + str(Combo2.get()) + ',' + '\n')
         TextBox.configure(state='disabled')
-        counter = int(Combo2.get())
-        for x in range(0, counter):
+        number = int(Combo2.get())
+        for x in range(0, number):
             if Combo1.get() == "Analgin":
                 List.append("a")
             elif Combo1.get() == "Elaksa piko":
@@ -43,16 +52,16 @@ def add():
 def undo():
     global List
     global index
-    global counter
+    global number
     if index != 0.0:
         TextBox.configure(state='normal')
         TextBox.delete(float(index), END)
         TextBox.insert(float(index), '\n')
         TextBox.configure(state='disabled')
-        l = len(List)
-        for x in range(0, counter):
-            l -= 1
-            List.pop(l)
+        listlen = len(List)
+        for x in range(0, number):
+            listlen -= 1
+            List.pop(listlen)
             x += 1
         index -= 1.0
 
@@ -77,7 +86,7 @@ def finish():
 
 
 options1 = [
-    "Choose option:",
+    "Choose medicine:",
     "Analgin",
     "Elaksa piko",
     "Espumizan",
@@ -87,28 +96,28 @@ options1 = [
 ]
 
 options2 = [
-    "Choose option:",
+    "Choose number:",
     "1",
     "2",
     "3",
 ]
 
-Combo1 = ttk.Combobox(root, font=20, state="readonly", value=options1)
+Combo1 = ttk.Combobox(root, width=30, font=40, state="readonly", value=options1)
 Combo1.current(0)
 Combo1.grid(row=0, column=1)
 
-Combo2 = ttk.Combobox(root, font=20, state="readonly", value=options2)
+Combo2 = ttk.Combobox(root, width=30, font=35, state="readonly", value=options2)
 Combo2.current(0)
 Combo2.grid(row=0, column=2)
 
-button_add = Button(root, text="Add to List", font=20, command=add)
-button_undo = Button(root, text="Undo", font=20, command=undo)
+button_add = Button(root, width=15, text="Add to List", font=30, command=add)
+button_undo = Button(root, width=15, text="Undo", font=30, command=undo)
 
 button_add.grid(row=0, column=4, pady=10)
 button_undo.grid(row=0, column=5, pady=10)
 
-button_clear = Button(root, text="Clear the list", font=20, command=clear)
-button_finish = Button(root, text="Finish", font=20, command=finish)
+button_clear = Button(root, width=15, text="Clear the list", font=40, command=clear)
+button_finish = Button(root, width=15, text="Finish", font=40, command=finish)
 
 button_clear.grid(row=2, column=1, pady=10)
 button_finish.grid(row=2, column=5, pady=10)
